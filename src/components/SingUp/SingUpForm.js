1e53f25497346235bde701837354d6a93d5e7a88
Entliczekperliczek <ewaperlinska55@gmail.com>
2020-03-03 22:27:36 +0100

    Add Sing Up Form and Sign Up Form Container

---
+++ src/components/SingUp/SingUpForm.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+const SignupForm = props => {
+  return (
+    <div>
+      Signup
+      <form onSubmit={props.handleSubmit}>
+        <label>User:</label>
+
+        <input
+          onChange={props.handleChange}
+          type="text"
+          name="username"
+          value={props.values.username}
+        />
+
+        <label>Name:</label>
+
+        <input
+          onChange={props.handleChange}
+          type="text"
+          name="name"
+          value={props.values.name}
+        />
+
+        <label>Surname:</label>
+
+        <input
+          onChange={props.handleChange}
+          type="text"
+          name="surname"
+          value={props.values.surname}
+        />
+
+        <label>Email:</label>
+
+        <input
+          onChange={props.handleChange}
+          type="email"
+          name="email"
+          value={props.values.email}
+        />
+
+        <label>Password:</label>
+        <input
+          onChange={props.handleChange}
+          type="password"
+          name="password"
+          value={props.values.password}
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default SignupForm;
